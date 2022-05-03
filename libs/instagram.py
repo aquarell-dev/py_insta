b83f9cef566ec02a65a3dd14947e890dbd9f7fdf
unknown <aquarell.dev@gmail.com>
2022-05-03 09:00:29 +0300

    add several methods

--- libs/instagram.py
+++ libs/instagram.py
@@ -1,4 +1,10 @@
 from selenium.common.exceptions import WebDriverException
+from selenium.common import exceptions
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from libs.core import Core, Docker, Proxy
 from config import dev_config
 
@@ -18,16 +24,59 @@ class Instagram:
         # self._driver = Docker().initialize_driver()
         # self._driver = Proxy(executable_path=dev_config.CHROMEDRIVER, proxy=dev_config.PROXY).initialize_driver()
 
+        self._wait = WebDriverWait(self._driver, 10)
+        self._ac = ActionChains(self._driver)
+
+    def __repr__(self) -> str:
+        return repr(f'Account. Login - {self._login}.')
+
     def login(self) -> None:
         if not self._sage_get('https://instagram.com'):
+            self._driver.quit()
             raise ConnectionError('Couldn\'t connect to instagram.')
 
-    def _like_posts(self) -> None:
+        locators = {
+            'login': (By.XPATH, ''),
+            'pass': (By.XPATH, ''),
+            'submit': (By.XPATH, ''),
+            'confirm_login': (By.XPATH, '')
+        }
+
+        try:
+            self._wait.until(EC.presence_of_element_located(locators['login']))
+            self._wait.until(EC.presence_of_element_located(locators['pass']))
+            self._wait.until(EC.presence_of_element_located(locators['submit']))
+        except WebDriverException:
+            self._driver.quit()
+            raise RuntimeError('Couldn\'t load login button.')
+
+        self._driver.find_element(locators['login']).send_keys(self._login)
+        self._driver.find_element(locators['pass']).send_keys(self._password)
+        self._driver.find_element(locators['submit']).click()
+
+        if self._does_element_exist(locators['confirm_login']):
+            self._driver.quit()
+            raise RuntimeError('Authentication error.')
+
+    def like_posts(self) -> None:
         """ Likes the three latest user's posts. """
 
-    def _subscribe(self) -> None:
+    def subscribe(self) -> None:
         """ Subscribes to a user. """
         pass
+
+    def _does_element_exist(self, locator) -> bool:
+        """ Returns True if element exists or else False. """
+        try:
+            self._wait.until(
+                EC.presence_of_element_located(
+                    locator
+                )
+            )
+        except (exceptions.TimeoutException, exceptions.StaleElementReferenceException):
+            return False
+
+        return True
 
     def _sage_get(self, url: str) -> bool:
         """ Goes to the page or else throws an error. """
--- pyinstabot.py
+++ pyinstabot.py
@@ -4,5 +4,8 @@ from config import user_config
 if __name__ == '__main__':
     instagram = Instagram(
         login=user_config.LOGIN,
-        password=user_config.PASSWORD
+        password=user_config.PASSWORD,
+        target=user_config.TARGET_LINK
     )
+
+    instagram.login()
