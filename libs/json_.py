fd6e54da629bdab7fa21f6d420257a31c2a2e3c1
aquarell <aquarell.dev@gmail.com>
2022-05-04 00:02:38 +0300

    add parsing input data from json

--- libs/json_.py
+++ libs/json_.py
@@ -1,5 +1,21 @@
 import json
 
-def read_file(file, **kwargs) -> dict:
+def read_file(file) -> dict:
     """ Reads a .json file, filtrates it and returns you a dict. """
-    pass
+    data = {}
+
+    with open(file, 'r') as f:
+        for key, value in json.load(f).items():
+            if not _key_exists(value, 'isFinished'): continue
+            if value['isFinished']: continue
+            data[key] = value
+
+    return data
+
+def _key_exists(key, value) -> bool:
+    try:
+        key[value]
+    except KeyError:
+        return False
+
+    return True
