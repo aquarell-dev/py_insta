88b4236c3f528e082e0ab137a619101fbd666bfa
aquarell <aquarell.dev@gmail.com>
2022-05-06 19:29:02 +0300

    upd json funcs

--- libs/json_.py
+++ libs/json_.py
@@ -1,16 +1,23 @@
 import json
 
 def read_file(file) -> dict:
-    """ Reads a .json file, filtrates it and returns you a dict. """
+    """ Reads a .json file and returns you a dict. """
     data = {}
 
-    with open(file, 'r') as f:
+    with open(file, 'r', encoding='utf-8') as f:
         for key, value in json.load(f).items():
-            if not _key_exists(value, 'isFinished'): continue
-            if value['isFinished']: continue
             data[key] = value
 
     return data
+
+def write_file(data: dict, file: str) -> bool:
+    try:
+        with open(file, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+    except:
+        return False
+
+    return True
 
 def _key_exists(key, value) -> bool:
     try:
@@ -19,3 +26,17 @@ def _key_exists(key, value) -> bool:
         return False
 
     return True
+
+def validate_json(data: dict, max_users: int) -> dict:
+    validated_data = {}
+
+    idx = 0
+
+    for key, value in data.items():
+        if not _key_exists(value, 'done'): continue
+        if value['done']: continue
+        idx += 1
+        validated_data[key] = value
+        if idx == max_users - 1: break
+
+    return validated_data
